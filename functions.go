de62436e0304322545f641bf1e51869cc80bcc8f
nalle631 <malteolofsson97@gmail.com>
2023-11-13 17:34:30 +0100

    added port and adress

--- functions.go
+++ functions.go
@@ -41,13 +41,13 @@ func GetClient(certFile string, keyFile string) *http.Client {
 	return client
 }
 
-func PublishService(requestBody Service) {
-
+func PublishService(requestBody Service, address string, port int) {
+	portSTR := strconv.Itoa(port)
 	payload, err := json.Marshal(requestBody)
 	if err != nil {
 		log.Fatal(err)
 	}
-	req, err := http.NewRequest("POST", "https://localhost:8443/serviceregistry/register", bytes.NewBuffer(payload))
+	req, err := http.NewRequest("POST", address+":"+portSTR, bytes.NewBuffer(payload))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -88,20 +88,23 @@ func GetServiceBody(interfaces []string, address string, port int, systemName st
 	return *requestBody
 }
 
-func RemoveService(service Service) {
+func RemoveService(service Service, address string, port int) {
+	portSTR := strconv.Itoa(port)
 	fmt.Println("Cleaning up before exit")
-	url := fmt.Sprintf("https://localhost:8443/serviceregistry/unregister?address=%s&port=%s&service_definition=%s&service_uri=%s&system_name=%s", service.ProviderSystem.Address, strconv.Itoa(service.ProviderSystem.Port), service.ServiceDefinition, service.ServiceUri, service.ProviderSystem.SystemName)
+	url := fmt.Sprintf(address+":"+portSTR+"/serviceregistry/unregister?address=%s&port=%s&service_definition=%s&service_uri=%s&system_name=%s", service.ProviderSystem.Address, strconv.Itoa(service.ProviderSystem.Port), service.ServiceDefinition, service.ServiceUri, service.ProviderSystem.SystemName)
 	req, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	client := GetClient("./usercert.pem", "./userkey.pem")
-	_, err = client.Do(req)
+	resp, err := client.Do(req)
 	fmt.Println("request sent")
 	if err != nil {
 		log.Panic("Error making HTTP request using client. ", err)
 	}
+
+	fmt.Println("## Response status:\n", resp.Status, resp.StatusCode)
 	fmt.Println("Service deleted")
 
 }
