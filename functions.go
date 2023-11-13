9a6ed9b6a95650b037e360d9ea6a51096582f6a9
nalle631 <malteolofsson97@gmail.com>
2023-11-13 15:04:06 +0100

    added removeService

--- functions.go
+++ functions.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type ProviderSystem struct {
@@ -15,7 +16,7 @@ type ProviderSystem struct {
 	SystemName string `json:"systemName"`
 }
 
-type RegServiceRequest struct {
+type Service struct {
 	Interfaces        []string       `json:"interfaces"`
 	ProviderSystem    ProviderSystem `json:"providerSystem"`
 	ServiceDefinition string         `json:"serviceDefinition"`
@@ -40,9 +41,17 @@ func GetClient(certFile string, keyFile string) *http.Client {
 	return client
 }
 
-func PublishService(requestBody RegServiceRequest) {
+func PublishService(requestBody Service) {
 
-	req := GetHTTPRequest("POST", "https://localhost:8443/serviceregistry/register", requestBody, "application/json")
+	payload, err := json.Marshal(requestBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req, err := http.NewRequest("POST", "https://localhost:8443/serviceregistry/register", bytes.NewBuffer(payload))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
 
 	client := GetClient("./usercert.pem", "./userkey.pem")
 	resp, err := client.Do(req)
@@ -55,7 +64,7 @@ func PublishService(requestBody RegServiceRequest) {
 	fmt.Println("## Response status:\n", resp.Status, resp.StatusCode)
 }
 
-func GetHTTPRequest(method string, url string, body RegServiceRequest, contentType string) *http.Request {
+func GetHTTPRequest(method string, url string, body Service, contentType string) *http.Request {
 	payload, err := json.Marshal(body)
 	if err != nil {
 		log.Fatal(err)
@@ -68,8 +77,8 @@ func GetHTTPRequest(method string, url string, body RegServiceRequest, contentTy
 	return req
 }
 
-func GetRegisterServiceRequestBody(interfaces []string, address string, port int, systemName string, serviceDefinition string, serviceUri string) RegServiceRequest {
-	requestBody := new(RegServiceRequest)
+func GetServiceBody(interfaces []string, address string, port int, systemName string, serviceDefinition string, serviceUri string) Service {
+	requestBody := new(Service)
 	requestBody.Interfaces = interfaces
 	requestBody.ProviderSystem.Address = address
 	requestBody.ProviderSystem.Port = port
@@ -77,4 +86,22 @@ func GetRegisterServiceRequestBody(interfaces []string, address string, port int
 	requestBody.ServiceDefinition = serviceDefinition
 	requestBody.ServiceUri = serviceUri
 	return *requestBody
+}
+
+func RemoveService(service Service) {
+	fmt.Println("Cleaning up before exit")
+	url := fmt.Sprintf("https://localhost:8443/serviceregistry/unregister?address=%s&port=%s&service_definition=%s&service_uri=%s&system_name=%s", service.ProviderSystem.Address, strconv.Itoa(service.ProviderSystem.Port), service.ServiceDefinition, service.ServiceUri, service.ProviderSystem.SystemName)
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := GetClient("./usercert.pem", "./userkey.pem")
+	_, err = client.Do(req)
+	fmt.Println("request sent")
+	if err != nil {
+		log.Panic("Error making HTTP request using client. ", err)
+	}
+	fmt.Println("Service deleted")
+
 }
