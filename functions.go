63902d2172da17fb763e91b00eac621aa3072e28
nalle631 <malteolofsson97@gmail.com>
2023-11-24 16:10:07 +0100

    added truststorepath to all methods

--- functions.go
+++ functions.go
@@ -67,13 +67,13 @@ func Hello() {
 	fmt.Println("Daniel-sama")
 }
 
-func EchoServiceRegistry(address string, port int, certFilePath string, keyFilePath string) ([]byte, error) {
+func EchoServiceRegistry(address string, port int, certFilePath string, keyFilePath string, truststorePath string) ([]byte, error) {
 	portSTR := strconv.Itoa(port)
 	req, errCreateRequest := http.NewRequest("GET", "https://"+address+":"+portSTR+"/serviceregistry/echo", nil)
 	if errCreateRequest != nil {
 		return nil, errCreateRequest
 	}
-	client := GetClient(certFilePath, keyFilePath)
+	client := GetClient(certFilePath, keyFilePath, truststorePath)
 	resp, errDoRequest := client.Do(req)
 	if errDoRequest != nil {
 		return nil, errDoRequest
@@ -87,22 +87,22 @@ func EchoServiceRegistry(address string, port int, certFilePath string, keyFileP
 
 }
 
-func RemoveServices(servicesToBeRemoved []Service, address string, port int, certFilePath string, keyFilePath string) {
+func RemoveServices(servicesToBeRemoved []Service, address string, port int, certFilePath string, keyFilePath string, truststorePath string) {
 	for _, service := range servicesToBeRemoved {
-		_, err := RemoveService(service, address, port, certFilePath, keyFilePath)
+		_, err := RemoveService(service, address, port, certFilePath, keyFilePath, truststorePath)
 		if err != nil {
 			fmt.Println("Error removing service: ", err)
 		}
 	}
 }
 
-func PublishServices(servicesToBeAdded []Service, address string, port int, certFilePath string, keyFilePath string) {
+func PublishServices(servicesToBeAdded []Service, address string, port int, certFilePath string, keyFilePath string, truststorePath string) {
 	for _, service := range servicesToBeAdded {
-		PublishService(service, address, port, certFilePath, keyFilePath)
+		PublishService(service, address, port, certFilePath, keyFilePath, truststorePath)
 	}
 }
 
-func GetClient(certFile string, keyFile string) *http.Client {
+func GetClient(certFile string, keyFile string, truststor string) *http.Client {
 	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
 		log.Panic("Certficate load error. ", err)
@@ -116,7 +116,7 @@ func GetClient(certFile string, keyFile string) *http.Client {
 	return client
 }
 
-func PublishService(requestBody Service, address string, port int, certFilePath string, keyFilePath string) {
+func PublishService(requestBody Service, address string, port int, certFilePath string, keyFilePath string, truststorePath string) {
 	portSTR := strconv.Itoa(port)
 	payload, err := json.Marshal(requestBody)
 	if err != nil {
@@ -129,7 +129,7 @@ func PublishService(requestBody Service, address string, port int, certFilePath 
 	}
 	req.Header.Set("Content-Type", "application/json")
 
-	client := GetClient(certFilePath, keyFilePath)
+	client := GetClient(certFilePath, keyFilePath, truststorePath)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Panic("Error making HTTP request using client. ", err)
@@ -143,7 +143,7 @@ func PublishService(requestBody Service, address string, port int, certFilePath 
 	fmt.Println("## Response status:\n", resp.Status, resp.StatusCode)
 }
 
-func RemoveService(service Service, address string, port int, certFilePath string, keyFilePath string) ([]byte, error) {
+func RemoveService(service Service, address string, port int, certFilePath string, keyFilePath string, truststorePath string) ([]byte, error) {
 	portSTR := strconv.Itoa(port)
 	urlFull := fmt.Sprintf("https://"+address+":"+portSTR+"/serviceregistry/unregister?address=%s&port=%s&service_definition=%s&service_uri=%s&system_name=%s", service.ProviderSystem.Address, strconv.Itoa(service.ProviderSystem.Port), service.ServiceDefinition, url.QueryEscape(service.ServiceUri), service.ProviderSystem.SystemName)
 	fmt.Println("URL: ", urlFull)
@@ -153,7 +153,7 @@ func RemoveService(service Service, address string, port int, certFilePath strin
 		return nil, errCreateRequest
 	}
 
-	client := GetClient(certFilePath, keyFilePath)
+	client := GetClient(certFilePath, keyFilePath, truststorePath)
 	resp, errDoRequest := client.Do(req)
 	if errDoRequest != nil {
 		return nil, errCreateRequest
@@ -166,7 +166,7 @@ func RemoveService(service Service, address string, port int, certFilePath strin
 
 }
 
-func RegisterSystem(rsrDTO System, address string, port int, certFilePath string, keyFilePath string) {
+func RegisterSystem(rsrDTO System, address string, port int, certFilePath string, keyFilePath string, truststorePath string) {
 	portSTR := strconv.Itoa(port)
 	payload, err := json.Marshal(rsrDTO)
 	if err != nil {
@@ -177,7 +177,7 @@ func RegisterSystem(rsrDTO System, address string, port int, certFilePath string
 		log.Fatal(err)
 	}
 	req.Header.Set("Content-Type", "application/json")
-	client := GetClient(certFilePath, keyFilePath)
+	client := GetClient(certFilePath, keyFilePath, truststorePath)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Panic("Error making HTTP request using client. ", err)
@@ -186,7 +186,7 @@ func RegisterSystem(rsrDTO System, address string, port int, certFilePath string
 	fmt.Println("## Response status:\n", resp.Status, resp.StatusCode)
 }
 
-func Orchestration(requestBody Orchestrate, address string, port int, certFilePath string, keyFilePath string) []byte {
+func Orchestration(requestBody Orchestrate, address string, port int, certFilePath string, keyFilePath string, truststorePath string) []byte {
 	portSTR := strconv.Itoa(port)
 	payload, err := json.Marshal(requestBody)
 	if err != nil {
@@ -198,7 +198,7 @@ func Orchestration(requestBody Orchestrate, address string, port int, certFilePa
 	}
 	req.Header.Set("Content-Type", "application/json")
 
-	client := GetClient(certFilePath, keyFilePath)
+	client := GetClient(certFilePath, keyFilePath, truststorePath)
 	resp, err := client.Do(req)
 	fmt.Println("request sent")
 	fmt.Println("requestbody: ", requestBody)
@@ -214,7 +214,7 @@ func Orchestration(requestBody Orchestrate, address string, port int, certFilePa
 	return body
 }
 
-func RemoveSystem(system System, address string, port int, certFilePath string, keyFilePath string) {
+func RemoveSystem(system System, address string, port int, certFilePath string, keyFilePath string, truststorePath string) {
 	portSTR := strconv.Itoa(port)
 	url := fmt.Sprintf("https://"+address+":"+portSTR+"/serviceregistry/unregister-system?address=%s&port=%s&system_name=%s", system.Address, strconv.Itoa(system.Port), system.SystemName)
 	req, err := http.NewRequest("DELETE", url, nil)
@@ -222,7 +222,7 @@ func RemoveSystem(system System, address string, port int, certFilePath string, 
 		log.Fatal(err)
 	}
 
-	client := GetClient(certFilePath, keyFilePath)
+	client := GetClient(certFilePath, keyFilePath, truststorePath)
 	resp, err := client.Do(req)
 	fmt.Println("request sent")
 	if err != nil {
